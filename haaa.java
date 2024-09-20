2cea13497fbb7be1975f05ea7722cbd36ab8be5c
unknown <nguyendinhtruong15012006@gmail.com>
2024-09-20 09:21:44 +0700

    baitap2

---
+++ haaa.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class haaa {
+    public static void main(String[] args) {
+
+       byte ASCIIcode;
+       Scanner keyboard = new Scanner(System.in);
+       System.out.println("Enter a string: ");
+       ASCIIcode = keyboard.nextByte();
+       System.out.println("ASCII code "+ ASCIIcode + "is " + (char)ASCIIcode);
+
+    }
+}
+
